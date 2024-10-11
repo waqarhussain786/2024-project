60daf7f6695c9c7f451e46b1119668aa904a275c
waqar hussain ujjan <wh751212@gmail.com>
2024-10-11 17:04:26 +0500

    navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SimpleLineChart from "./App"
-import BiaxialLineChart from './biaxiallinechart';
+// import SimpleLineChart from "./App"
+// import BiaxialLineChart from './biaxiallinechart';
+import OffcanvasExample from "./navBar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SimpleLineChart />
-    <BiaxialLineChart />
+    <OffcanvasExample />
+    {/* <SimpleLineChart /> */}
+    {/* <BiaxialLineChart /> */}
   </React.StrictMode>
 );
